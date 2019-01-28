a9a08d14c4addc6064b11e9593a5ec7b2c4f6295
ChungTM1209 <minhchung.ctk17@gmail.com>
2019-01-28 15:06:27 +0700

    BC2_SS6_Bai2

--- Square.php
+++ Square.php
@@ -1,16 +1,15 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: dungduong
- * Date: 11/21/18
- * Time: 17:16
- */
-include_once ('Rectangle.php');
 
-class Square extends Rectangle
+include_once ('Rectangle.php');
+include_once 'Colorable.php';
+class Square extends Rectangle implements Colorable
 {
     public function __construct($name, $width)
     {
         parent::__construct($name, $width, $width, $width);
+    }
+    public function howToColor()
+    {
+        return "Colour all four sides.";
     }
 }
--- index.php
+++ index.php
@@ -1,27 +1,18 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: dungduong
- * Date: 11/21/18
- * Time: 17:17
- */
-include_once ('Circle.php');
-include_once ('Cylinder.php');
-include_once ('Rectangle.php');
-include_once ('Square.php');
 
-$circle = new Circle('Circle 01', 3);
-echo 'Circle area: ' . $circle->calculateArea() . '<br />';
-echo 'Circle perimeter: ' . $circle->calculatePerimeter() . '<br />';
+include_once('Circle.php');
+include_once('Cylinder.php');
+include_once('Rectangle.php');
+include_once('Square.php');
 
-$cylinder = new Cylinder('Cylinder 01', 3 , 4);
-echo 'Cylinder area: ' . $cylinder->calculateArea() . '<br />';
-echo 'Cylinder perimeter: ' . $cylinder->calculatePerimeter() . '<br />';
+$shapes[0] = new Circle('Circle 01', 3);
+$shapes[1] = new Cylinder('Cylinder 01', 3, 4);
+$shapes[2] = new Rectangle('Rectangle 01', 3, 4);
+$shapes[3] = new Square('Square 01', 4, 4, 4);
 
-$rectangle = new Rectangle('Rectangle 01', 3 , 4);
-echo 'Rectangle area: ' . $rectangle->calculateArea() . '<br />';
-echo 'Rectangle perimeter: ' . $rectangle->calculatePerimeter() . '<br />';
-
-$square = new Square('Square 01', 4 , 4, 4);
-echo 'Rectangle area: ' . $square->calculateArea() . '<br />';
-echo 'Rectangle perimeter: ' . $square->calculatePerimeter() . '<br />';
+foreach ($shapes as $shape) {
+    echo $shape->name . "<br>" . "Area: " . $shape->calculateArea() . "<br>";
+    if ($shape instanceof Square) {
+        echo $shape->howToColor();
+    }
+}
